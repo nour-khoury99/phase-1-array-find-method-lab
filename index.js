c63b85c3f634b86eefe71bf26c3284106e1b7db9
nour khoury <khourynour79@gmail.com>
2022-09-19 10:45:45 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+/*Array.prototype.filter() :want a list of all elements that meet in one donsition
+  Array.prototype.find() : take a callback function
+  Array.prototype.indexOf() : is the only one that doesn't takes a callback function
+ */
+
+
+  const record = [
+    {year:"2018" , result:"L"},
+    {year:"2017",result:"W"},
+    {year:"2016",result:"N/A"}
+  ]
+
+
+  function superbowlWin(element,result){
+
+   if( element.result == "W" ){
+        return "win";
+   }else{
+    return undefined;
+   }
+    
+  }
+
+  console.log(record.find(superbowlWin));
